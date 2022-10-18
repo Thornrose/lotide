a46edd27a8898e606d0d33842edc1a8941b7db4a
Cheever Esler <ames.esler@gmail.com>
2022-10-17 21:27:47 -0300

    corrected template literal error

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@
 // Function implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: " + ${actual} + " === " + ${expected}`);
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
     return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
@@ -10,7 +10,7 @@ const assertEqual = function(actual, expected) {
 
 // Test code
 assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
+//assertEqual(1, 1);
 // assertEqual("words and words", "words and worDs");
 // assertEqual(23, 22);
 // assertEqual("Yes", "Yes");
