089c29a6cc5cfc11a63f194831a2721a32827110
Cheever Esler <ames.esler@gmail.com>
2022-10-17 21:22:58 -0300

    updated with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 // Function implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    return console.log(`✅✅✅ Assertion Passed: " + ${actual} + " === " + ${expected}`);
   } else if (actual !== expected) {
-    return console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+    return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
