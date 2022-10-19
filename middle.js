3d7934374fb05e6199a8e5dbf4dabacc52c6fc92
Cheever Esler <ames.esler@gmail.com>
2022-10-19 15:04:23 -0300

    first cleanup pass

--- middle.js
+++ middle.js
@@ -10,7 +10,7 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-
+// testing functions
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     return console.log(`✅✅✅ Array Assertion Passed: ${array1} === ${array2}`);
@@ -19,17 +19,6 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-// Goal: take in one array and return the middle element(s) of it as a new array
-// consider length - if array length is even, will display two elements. If odd, will display one.
-// no middle for arrays with only one or two elements. return empty arrays
-// steps:
-// read array
-// measure array length
-// decide what to do if length is even or odd
-// extract value from middle
-// display as new array
-// deal with edge cases: empty array, one element, two elements
-
 const middle = function(array) {
   const arrayMiddle = [];
   const midCount = array.length % 2 === 0 ? 2 : 1;
@@ -37,23 +26,18 @@ const middle = function(array) {
   if (array.length <= 2) {
     return arrayMiddle;
   }
-  //console.log("middlemost index", midPoint);
-  //console.log("How many values should be displayed:", midCount);
+  
   arrayMiddle.push(array[midPoint - 1]);
-  //console.log(arrayMiddle);
   if (midCount === 2) {
     arrayMiddle.push(array[midPoint]);
-    //console.log(arrayMiddle);
-  } else if (midCount === 1) {
   }
   return arrayMiddle;
 }
 
 // pre-flight test code
-middle([1, 2, 3, 4, 5]);
-middle([1, 2, 3, 4]);
-middle([1, 2, 3, 4, 5, 6]);
-
+// middle([1, 2, 3, 4, 5]);
+// middle([1, 2, 3, 4]);
+// middle([1, 2, 3, 4, 5, 6]);
 
 // test code - should all pass
 assertArraysEqual(middle([1]), []);
@@ -63,6 +47,6 @@ assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 
-// some fails
+// some fail tests
 assertArraysEqual(middle([1, 2]), [1]);
 assertArraysEqual(middle([2, 6, 2, 8]), [1, 2, 3, 4]);
