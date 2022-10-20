074d8a85cbc893e877940d80833998b6080593ea
Cheever Esler <ames.esler@gmail.com>
2022-10-19 22:03:47 -0300

    completed findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// goal: function takes in object and value.
+// so, this will use for..in.
+// go through keys using variable, checking values.
+// when you find the value you're looking for, return the actual name of the key.
+
+const findKeyByValue = function(sourceObj, goalVal) {
+  for (let key in sourceObj) {
+    if (sourceObj[key] === goalVal) {
+      return key;
+    }
+  }
+}
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
