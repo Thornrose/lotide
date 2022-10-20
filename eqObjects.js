1db44b93fd51d0efa6d89e751e2d1e1d5bb789e9
Cheever Esler <ames.esler@gmail.com>
2022-10-19 23:06:37 -0300

    initial solution to be refactored

--- eqObjects.js
+++ eqObjects.js
@@ -8,9 +8,36 @@ const assertEqual = function(actual, expected) {
 
 // goal: take in two OBJECTS and return true or false based on perfect === match
 // (identical keys with identical values)
+// simple: two objects are equal when they have same number of keys and key values match keys
+// so, steps:
+// read object length?
+// read keys in both objects.
+// read values of all keys.
+// confirm keys match.
 
 const eqObjects = function(obj1, obj2) {
-
+  let counter1 = 0;
+  let counter2 = 0;
+  for (let item in obj1) {
+    
+    counter1++;
+    if (obj1[item] !== obj2[item]) {
+      return false;
+    }
+  //   // console.log("key is", item);
+  //   // console.log("value is", obj1[item]);
+   }
+  // // console.log("counter1 counts", counter1);
+    for (let item in obj2) {
+    counter2++;
+  //   // console.log("key is", item);
+  //   // console.log("value is", obj2[item]);
+    }
+  // // console.log("counter2 counts", counter2);
+   if (counter1 !== counter2) {
+    return false;
+  }
+  return true;
 };
 
 
